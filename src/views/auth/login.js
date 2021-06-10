8af117eace9ba9ecc4e203aec8596c06644f87a1
Dong Phung <dong.phung@i3international.com>
2021-06-10 03:58:16 +0000

    * fix login screen: styles and forget password button

--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -4,7 +4,7 @@ import {
   StyleSheet,
   Text,
   Image,
-  ActivityIndicator,
+  Linking,
   Alert,
   Dimensions,
 } from 'react-native';
@@ -22,7 +22,8 @@ import Button from '../../components/controls/Button';
 import {isValidHttpUrl} from '../../util/general';
 
 import {Domain} from '../../consts/misc';
-import styles from '../../styles/scenes/login.style';
+import APP_INFO from '../../consts/appInfo';
+import variable from '../../styles/variables';
 import CMSColors from '../../styles/cmscolors';
 import {I3_Logo} from '../../consts/images';
 import {Login as LoginTxt} from '../../localization/texts';
@@ -33,6 +34,7 @@ import {Login as LoginTxt} from '../../localization/texts';
 // const validators = validatejs.validators;
 // <!-- END CONSTS -->
 // ----------------------------------------------------
+const {width} = Dimensions.get('window');
 
 class LoginView extends Component {
   constructor(props) {
@@ -205,12 +207,7 @@ class LoginView extends Component {
     // );
 
     return (
-      <View
-        style={{
-          flex: 1,
-          paddingLeft: width * 0.1,
-          paddingRight: width * 0.1,
-        }}>
+      <View style={styles.viewContainer}>
         <KeyboardAwareScrollView
           // keyBoardShouldPersistTaps="always"
           ref={r => {
@@ -224,18 +221,9 @@ class LoginView extends Component {
             this._refs.password,
           ]}>
           <View style={{flex: 1}}>
-            <View
-              style={{
-                flex: 5,
-                marginTop: 16,
-                flexDirection: 'column',
-                alignItems: 'flex-end',
-              }}>
+            <View style={styles.closeButtonContainer}>
               <Button
-                style={{
-                  // width: width * 0.1,
-                  alignItems: 'center',
-                }}
+                style={styles.closeButton}
                 enable={true}
                 type={'flat'}
                 iconCustom={'clear-button'}
@@ -247,29 +235,19 @@ class LoginView extends Component {
                 onPress={this.onBack}
               />
             </View>
-            <View style={{flex: 20}}>
+            <View style={styles.logoContainer}>
               <Image
                 source={I3_Logo}
-                style={{
-                  tintColor: CMSColors.Dark_Blue,
-                  width: width * 0.5,
-                  height: '100%',
-                }}
+                style={styles.logo}
                 resizeMode="contain"
               />
             </View>
-            <View
-              style={{
-                flex: 10,
-                paddingTop: 28,
-              }}>
-              <Text style={{fontSize: 24, fontWeight: 'normal'}}>
+            <View style={styles.textContainer}>
+              <Text style={styles.textTitle}>
                 {LoginTxt.title}
-                <Text style={{fontWeight: 'bold'}}>{LoginTxt.titleBold}</Text>
+                <Text style={styles.textBold}>{LoginTxt.titleBold}</Text>
               </Text>
-              <Text style={{fontSize: 16, paddingTop: 14}}>
-                {LoginTxt.description}
-              </Text>
+              <Text style={styles.textDesc}>{LoginTxt.description}</Text>
             </View>
             <View
               style={[
@@ -279,7 +257,7 @@ class LoginView extends Component {
                 {flex: 35},
               ]}>
               {/* <View style={styles.centerContent}> */}
-              <View style={{alignItems: 'center'}}>
+              <View style={styles.inputsContainer}>
                 <InputTextIcon
                   ref={r => (this._refs.domain = r)}
                   name="domain"
@@ -348,18 +326,9 @@ class LoginView extends Component {
                 />
               </View>
             </View>
-            <View
-              style={{
-                flex: 30,
-                // paddingTop: 20,
-                alignItems: 'center',
-                flexDirection: 'column',
-              }}>
+            <View style={styles.buttonsContainer}>
               <Button
-                style={{
-                  width: '100%',
-                  marginTop: 40,
-                }}
+                style={styles.buttonLogin}
                 caption="LOGIN"
                 type="primary"
                 captionStyle={{}}
@@ -370,14 +339,13 @@ class LoginView extends Component {
                 }
               />
               <Button
-                style={{
-                  width: '100%',
-                  marginTop: 15,
-                }}
+                style={styles.buttonPassword}
                 caption="FORGOT PASSWORD?"
                 type="flat"
                 captionStyle={{}}
-                onPress={this.onLogin}
+                onPress={() => {
+                  Linking.openURL(APP_INFO.ContactUrl);
+                }}
                 enable={true}
               />
             </View>
@@ -387,5 +355,66 @@ class LoginView extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    paddingLeft: width * 0.1,
+    paddingRight: width * 0.1,
+  },
+  closeButtonContainer: {
+    flex: 5,
+    marginTop: 16,
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  },
+  closeButton: {
+    // width: width * 0.1,
+    alignItems: 'center',
+  },
+  logo: {
+    tintColor: CMSColors.Dark_Blue,
+    width: width * 0.5,
+    height: '100%',
+  },
+  logoContainer: {flex: 20},
+  centerContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 7,
+    paddingRight: 7,
+    flex: 1,
+    flexDirection: 'column',
+  },
+  textContainer: {
+    flex: 10,
+    paddingTop: 28,
+  },
+  textTitle: {fontSize: 24, fontWeight: 'normal'},
+  textBold: {fontWeight: 'bold'},
+  textDesc: {fontSize: 16, paddingTop: 14},
+  inputsContainer: {alignItems: 'center'},
+  buttonsContainer: {
+    flex: 30,
+    // paddingTop: 20,
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  buttonLogin: {
+    width: '100%',
+    marginTop: 40,
+  },
+  buttonPassword: {
+    width: '100%',
+    marginTop: 15,
+  },
+  content: {
+    maxWidth: variable.deviceWidth > 370 ? 400 : variable.deviceWidth,
+    backgroundColor: CMSColors.transparent,
+  },
+  captionStyle: {
+    color: CMSColors.textButtonLogin,
+  },
+});
 
 export default inject('userStore', 'appStore')(observer(LoginView));
