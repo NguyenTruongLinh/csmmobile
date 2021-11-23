2444c828858fa9f528bb80f65b370708ce71179c
Hai Le <hai.le@i3international.com>
2021-11-23 09:11:02 +0000

    * fix GUI bugs in OAM screen - Acknowledge: #46845, #46844, #46863, #46843

--- src/views/oam/widget/AcknowledgePopup.js
+++ src/views/oam/widget/AcknowledgePopup.js
@@ -62,16 +62,11 @@ class AcknowledgePopup extends Component {
     const {oamStore} = this.props;
     return (
       <View
-        style={[
-          {
-            flex: oamStore.data.kAlertType < 3 ? 3 : 2,
-            borderBottomWidth: 2,
-            borderBottomColor: 'rgba(189,189,189,0.54)',
-            flexDirection: 'column',
-          },
-        ]}>
+        style={{
+          flexDirection: 'column',
+        }}>
         {warningMessages.map((msg, index) => {
-          return index != 2 || oamStore.data.kAlertType < 3 ? (
+          return index != 1 || oamStore.data.kAlertType < 3 ? (
             <Ripple
               style={[styles.radiobuttonContainer]}
               onPress={() => {
@@ -84,7 +79,7 @@ class AcknowledgePopup extends Component {
                     : 'circle-o'
                 }
                 size={24}
-                color={CMSColors.PrimaryColor}
+                color={CMSColors.PrimaryActive}
               />
               <Text style={{paddingLeft: 10}}> {warningMessages[index]}</Text>
             </Ripple>
@@ -95,6 +90,8 @@ class AcknowledgePopup extends Component {
   };
 
   render() {
+    const {width, height} = Dimensions.get('window');
+    const screenWidth = width;
     const {oamStore} = this.props;
     return (
       <Modal
@@ -119,83 +116,56 @@ class AcknowledgePopup extends Component {
         <View style={{flex: 1, flexDirection: 'column', padding: 0}}>
           <TouchableOpacity
             style={{
-              flex: oamStore.data.kAlertType < 3 ? 3 : 4,
+              flex: 1,
               backgroundColor: CMSColors.borderColor,
             }}
             activeOpacity={1}
             onPress={() => oamStore.setAckPopupVisibility(false)}
           />
-          <View
-            style={[
-              styles.modalcontainer,
-              {
-                flex: oamStore.data.kAlertType < 3 ? 7 : 6,
-              },
-            ]}>
-            <View style={[styles.modalcontent, {flex: 3}]}>
+          <View style={styles.modalcontainer}>
+            <View style={styles.modalHeader}>
+              <IconCustom
+                disabled={true}
+                style={[
+                  styles.icon,
+                  {color: oamStore.data.kAlertType >= 3 ? 'red' : 'orange'},
+                ]}
+                size={normalize(40)}
+                name={
+                  oamStore.data.kAlertType >= 3 ? 'o-warning' : 'warning-sign'
+                }
+              />
+            </View>
+            {this.renderRadioMessages()}
+            <View style={styles.popupFooterButtons}>
               <View
                 style={{
                   flex: 1,
-                  flexDirection: 'column',
-                  alignContent: 'center',
-                  alignItems: 'center',
                   justifyContent: 'center',
                 }}>
-                <IconCustom
-                  disabled={true}
-                  style={[
-                    styles.icon,
-                    {color: oamStore.data.kAlertType >= 3 ? 'red' : 'orange'},
-                  ]}
-                  size={normalize(40)}
-                  name={'round-error-symbol'}
+                <Button
+                  style={styles.footerButton}
+                  type="flat"
+                  caption="Cancel"
+                  enable={true}
+                  captionStyle={styles.buttonCaption}
+                  onPress={() => oamStore.setAckPopupVisibility(false)}
                 />
               </View>
-            </View>
-            <View
-              style={{
-                flexDirection: 'column',
-                alignContent: 'center',
-                justifyContent: 'center',
-                flex: 8,
-              }}>
-              {this.renderRadioMessages()}
+              <View style={{width: 10}}></View>
               <View
-                style={[
-                  styles.popupcomponent,
-                  {paddingLeft: 20, paddingRight: 20},
-                ]}>
-                <View
-                  style={{
-                    flex: 4,
-                    marginBottom: 20,
-                  }}>
-                  <Button
-                    style={[styles.buttonIgnore, {backgroundColor: 'white'}]}
-                    type="Custom"
-                    caption="Cancel"
-                    IconStyleEnable={[{color: CMSColors.PrimaryText}]}
-                    styleCaption={styles.buttonCaption}
-                    enable={true}
-                    iconCustom="clear-button"
-                    onPress={() => oamStore.setAckPopupVisibility(false)}
-                  />
-                </View>
-                <View
-                  style={{
-                    flex: 6,
-                    paddingLeft: 10,
-                    marginBottom: 20,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <Button
-                    enable={true}
-                    type={'primary'}
-                    caption="Acknowledge"
-                    onPress={this.onAckPostPress.bind(this)}
-                  />
-                </View>
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                }}>
+                <Button
+                  style={styles.footerButton}
+                  enable={true}
+                  type={'primary'}
+                  caption="Acknowledge"
+                  captionStyle={styles.buttonCaption}
+                  onPress={this.onAckPostPress.bind(this)}
+                />
               </View>
             </View>
           </View>
@@ -206,17 +176,6 @@ class AcknowledgePopup extends Component {
 }
 
 const styles = StyleSheet.create({
-  radiobuttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignContent: 'center',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingLeft: 24,
-    paddingRight: 24,
-    //marginTop:20,
-    marginRight: 20,
-  },
   modalcontainer: {
     justifyContent: 'flex-start',
     alignItems: 'center',
@@ -225,43 +184,44 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
   },
-  modalcontent: {
-    height: 50,
+  modalHeader: {
+    height: 80,
     alignContent: 'center',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
     backgroundColor: 'transparent',
     borderBottomColor: 'rgb(204, 204, 204)',
+    paddingTop: 20,
   },
   radiobuttonContainer: {
-    flex: 1,
     flexDirection: 'row',
     alignContent: 'center',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingLeft: 24,
+    paddingLeft: 18,
     paddingRight: 24,
-    //marginTop:20,
-    marginRight: 20,
+    marginTop: 20,
+    marginRight: 12,
   },
-  popupcomponent: {
+  popupFooterButtons: {
+    marginTop: 24,
+    height: 80,
     flexDirection: 'row',
-    flex: 1,
-    paddingTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
+    padding: 18,
+    marginBottom: 24,
   },
-  buttonIgnore: {
+  footerButton: {
+    maxHeight: 50,
     flex: 1,
-    backgroundColor: CMSColors.PrimaryColor,
+    borderColor: CMSColors.PrimaryActive,
+    borderWidth: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 10,
   },
   buttonCaption: {
-    color: '#fff',
-    fontSize: normalize(13),
+    fontSize: normalize(14),
+    fontWeight: 'bold',
   },
 });
 
