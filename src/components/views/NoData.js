77813f460d0cc51c520689d8d4196fc052bb7f32
Dong Phung <dong.phung@i3international.com>
2021-12-03 02:31:11 +0000

    - nodata view

---
+++ src/components/views/NoData.js
@@ -0,0 +1,65 @@
+'use strict';
+
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+import PropTypes from 'prop-types';
+
+import {No_Data} from '../../consts/images';
+
+import CMSColors from '../../styles/cmscolors';
+import {COMMON as COMMON_TXT} from '../../localization/texts';
+
+export default class NoDataView extends React.Component {
+  static propTypes = {
+    height: PropTypes.number,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: 0,
+    };
+  }
+
+  onLayout = ({nativeEvent}) => {
+    const {width, height} = nativeEvent.layout;
+
+    this.setState({height});
+  };
+
+  render() {
+    return (
+      <View
+        style={[
+          styles.noDataContainer,
+          this.props.style,
+          //{height: this.props.height}
+        ]}
+        onLayout={this.onLayout}>
+        <Image source={No_Data} style={styles.noDataImg}></Image>
+        <Text style={styles.noDataTxt}>{COMMON_TXT.NO_DATA}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  noDataContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  noDataImg: {
+    width: 100,
+    height: 100,
+  },
+  noDataTxt: {
+    marginTop: 12,
+    paddingBottom: 50,
+    fontSize: 16,
+    color: CMSColors.PrimaryText,
+  },
+});
