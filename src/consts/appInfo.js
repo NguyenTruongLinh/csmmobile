f5e660c9d1292cc6272e1b802b2b2b025c77efc5
Dong Phung <dong.phung@i3international.com>
2022-03-04 04:56:42 +0000

    - update version to 3.0.0.62

--- src/consts/appInfo.js
+++ src/consts/appInfo.js
@@ -1,8 +1,8 @@
 const APP_INFO = {
   Title: 'Cloud Managed Services',
   Name: 'CMS Mobile 3.0',
-  Version: '3.0.0.61',
-  BuiltDate: 'Mar 03, 2022',
+  Version: '3.0.0.62',
+  BuiltDate: 'Mar 04, 2022',
   CopyRight: 'Copyright © 2022 i3 International Inc.',
   // AppId: '4d53bce03ec34c0a911182d4c228ee6c',
   AppId: '89ab5a91edf94caeae6f5a38e1cc3c26',
